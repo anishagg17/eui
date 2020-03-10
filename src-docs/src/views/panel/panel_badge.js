01f34e24b922f170bc22a069354337b13e7b7624
Anish Aggarwal <anish17122000@gmail.com>
2020-03-11 02:23:50 +0530

    panel,   working

--- src-docs/src/views/panel/panel_badge.js
+++ src-docs/src/views/panel/panel_badge.js
@@ -6,23 +6,26 @@ import {
   EuiFlexItem,
 } from '../../../../src/components';
 
-const badges = [null, 'Beta', 'Lab'];
 
-const panelNodes = badges.map(function(item, index) {
-  return (
-    <EuiFlexItem key={index}>
-      <EuiPanel
-        betaBadgeLabel={badges[index]}
-        betaBadgeTooltipContent={
-          badges[index]
-            ? 'This module is not GA. Please help us by reporting any bugs.'
-            : undefined
-        }
-        onClick={() => window.alert('Card clicked')}>
-        I am some panel content
+
+export default () => {
+  const badges = [null, 'Beta', 'Lab'];
+
+  const panelNodes = badges.map(function (item, index) {
+    return (
+      <EuiFlexItem key={index}>
+        <EuiPanel
+          betaBadgeLabel={badges[index]}
+          betaBadgeTooltipContent={
+            badges[index]
+              ? 'This module is not GA. Please help us by reporting any bugs.'
+              : undefined
+          }
+          onClick={() => window.alert('Card clicked')}>
+          I am some panel content
       </EuiPanel>
-    </EuiFlexItem>
-  );
-});
-
-export default () => <EuiFlexGroup gutterSize="l">{panelNodes}</EuiFlexGroup>;
+      </EuiFlexItem>
+    );
+  });
+  return <EuiFlexGroup gutterSize="l">{panelNodes}</EuiFlexGroup>
+};
