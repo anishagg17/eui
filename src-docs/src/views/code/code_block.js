7991e30e14d9eccdf68de4ff5e6b07b971fb8678
Anish Aggarwal <anish17122000@gmail.com>
2020-03-23 21:12:43 +0530

    code working

--- src-docs/src/views/code/code_block.js
+++ src-docs/src/views/code/code_block.js
@@ -2,30 +2,19 @@ import React from 'react';
 
 import { EuiCodeBlock, EuiSpacer } from '../../../../src/components';
 
-const htmlCode = `<!--I'm an example of HTML-->
-<div>
-  <h1>Title</h1>
-</div>
-`;
-
-const jsCode = require('!!raw-loader!./code_block');
-
-export default () => (
+export default () => {
+  const htmlCode = `<!--I'm an example of HTML-->
   <div>
-    <EuiCodeBlock language="html">{htmlCode}</EuiCodeBlock>
-
-    <EuiSpacer />
-
-    <EuiCodeBlock
-      language="js"
-      fontSize="m"
-      paddingSize="m"
-      color="dark"
-      overflowHeight={300}
-      isCopyable>
-      {jsCode}
-    </EuiCodeBlock>
-
-    <EuiSpacer />
+    <h1>Title</h1>
   </div>
-);
+  `;
+  return (
+    <div>
+      <EuiCodeBlock language="html">{htmlCode}</EuiCodeBlock>
+
+      <EuiSpacer />
+
+      <EuiSpacer />
+    </div>
+  );
+};
